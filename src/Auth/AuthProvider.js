ee040b1173febdc416cf6a7cddcb03ecf2425dd0
kowsarahammed80 <kowsarahammed80@gmail.com>
2022-11-25 12:21:13 +0600

    privateRoute-set

--- src/Auth/AuthProvider.js
+++ src/Auth/AuthProvider.js
@@ -1,6 +1,7 @@
 import React, { createContext, useEffect, useState } from 'react';
 import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, onAuthStateChanged, signOut, updateProfile} from 'firebase/auth'
 import app from '../Firebase/Firebase.config';
+import Loading from '../Sheard/Loading/Loading';
 
 export const AuthContext = createContext() 
 
@@ -13,18 +14,22 @@ export const AuthContext = createContext()
   const [loading, setLoading] = useState(true)
 
   const providerLogIn = (provider) => {
+    setLoading(true)
     return signInWithPopup(auth,provider)
   }
 
   const signUp = (email, password) => {
+    setLoading(true)
      return createUserWithEmailAndPassword(auth, email, password) 
   }
 
   const logIn = (email, password) => {
+    setLoading(true)
     return signInWithEmailAndPassword(auth, email, password)
   }
 
   const logOut = () =>{
+    setLoading(true)
     return signOut(auth)
   }
 
@@ -39,6 +44,10 @@ export const AuthContext = createContext()
      return unsubscribe;
    } 
   },[])
+
+  if(loading){
+    return <Loading/>
+  }
     
   const authInfo = {
     signUp , logIn , loading ,user, providerLogIn, logOut
